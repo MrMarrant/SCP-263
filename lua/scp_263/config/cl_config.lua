01fc29f8f53161518426b1f0854ccafbc1a2e381
MrMarrant <laughymclaugher@gmail.com>
2023-09-01 16:54:49 +0200

    [FEATURE] Default Convar value CL

--- lua/scp_263/config/cl_config.lua
+++ lua/scp_263/config/cl_config.lua
@@ -1,6 +1,11 @@
 SCP_263_CONFIG.ScrW = ScrW()
 SCP_263_CONFIG.ScrH = ScrH()
 
+--? Default Value Convar
+SCP_263_CONFIG.ClientTimeToBurn = 42
+SCP_263_CONFIG.ClientTimetoAnswer = 45
+SCP_263_CONFIG.ClientMaximumDelimitationGame = 200
+
 surface.CreateFont( "SCP263_TitleQuestion", {
     font = "Arial",
     size = 70,
