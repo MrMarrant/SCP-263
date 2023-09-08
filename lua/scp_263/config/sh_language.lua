754ac08df0aea960fd4a73937a83e82a6c90a591
MrMarrant <laughymclaugher@gmail.com>
2023-09-08 12:24:55 +0200

    [UPDATE] Add question

--- lua/scp_263/config/sh_language.lua
+++ lua/scp_263/config/sh_language.lua
@@ -36,3 +36,159 @@ function SCP_263.GetTranslation(name)
     end
     return string.format( SCP_263_LANG[langUsed][ name ] or "Not Found" )
 end
+
+-----------GENERATE JSON FILE QUESTION-------------------
+
+--? Default Questions, Edit the JSON file created instead of here.
+SCP_263_CONFIG.QuestionList = {}
+local QuestionList = {}
+
+QuestionList["fr"] = {
+	{
+		question = "Dans quel Site est contenu SCP-682 ?",
+		correct_answer = "a",
+		response_a = "Site 12",
+		response_b = "Site 20",
+		response_c = "Site 32",
+		response_d = "Aucun",
+	},
+	{
+		question = "Que signifie le protocle XZ ?",
+		correct_answer = "b",
+		response_a = "Une fin du monde",
+		response_b = "Plus de café",
+		response_c = "Fin de l'Humanité",
+		response_d = "Fin de la Civilisation",
+	},
+	{
+		question = "Qu'est donc SCP 420-J",
+		correct_answer = "d",
+		response_a = "Un poisson alien",
+		response_b = "Une statue avec un chapeau mexicain",
+		response_c = "Un ancien président français anormal",
+		response_d = "Un énorme pétard",
+	},
+    {
+		question = "À qui obéit le comité d'éthique ?",
+		correct_answer = "a",
+		response_a = "Le conseil O5",
+		response_b = "Les représentants du conseil 05",
+		response_c = "L'administrateur du site dans lequel ils opèrent",
+		response_d = "Le Superviseur",
+	},
+    {
+		question = "Quel est la mission principale de la FIM Lambda-5",
+		correct_answer = "c",
+		response_a = "L'exploration de zone anormal",
+		response_b = "La gestion des attaques terroristes anormal",
+		response_c = "Le confinement d'anomalies",
+		response_d = "L'entrainement des agents de terrain",
+	},
+    {
+		question = "Dans quelle circonstance doit être appelé la FIM Lambda-5 ?",
+		correct_answer = "a",
+		response_a = "Lors d'un code MAGENTA",
+		response_b = "Lors d'un code NOIR",
+		response_c = "Lors d'une Initiatives de Réponse Automatisée de type RA-1",
+		response_d = "Lors d'une rébellion des membres de Classe-D",
+	},
+    {
+		question = "Pourquoi le comité d'éthique existe-t-il ?",
+		correct_answer = "",
+		response_a = "Ils n'ont pas d'utilité, ils sont inutile",
+		response_b = "Pour protéger les membres de la fondation",
+		response_c = "Pour sauver le personnel de Classe-D",
+		response_d = "Pour poser les limites à la fondation",
+	},
+    {
+		question = "Si une révolte du personnel éclate quel Fim doit intervenir ?",
+		correct_answer = "",
+		response_a = "",
+		response_b = "",
+		response_c = "",
+		response_d = "",
+	},
+    {
+		question = "",
+		correct_answer = "",
+		response_a = "",
+		response_b = "",
+		response_c = "",
+		response_d = "",
+	},
+    {
+		question = "",
+		correct_answer = "",
+		response_a = "",
+		response_b = "",
+		response_c = "",
+		response_d = "",
+	},
+    {
+		question = "",
+		correct_answer = "",
+		response_a = "",
+		response_b = "",
+		response_c = "",
+		response_d = "",
+	},
+
+
+}
+
+QuestionList["en"] = {
+    {
+		question = "In which Site is SCP-682 contained?",
+		correct_answer = "a",
+		response_a = "Site 12",
+		response_b = "Site 20",
+		response_c = "Site 32",
+		response_d = "None",
+	},
+	{
+		question = "What does the XZ protocol mean?",
+		correct_answer = "b",
+		response_a = "An end of the world",
+		response_b = "No more coffee",
+		response_c = "End of Humanity",
+		response_d = "End of Civilization",
+	},
+	{
+		question = "Who smoked a huge banger?",
+		correct_answer = "d",
+		response_a = "Dr Bright",
+		response_b = "John Atand",
+		response_c = "Chirac",
+		response_d = "You",
+	},
+}
+
+/*
+* Allows to return the data of a file.
+* @string path File path.
+*/
+local function GetDataFromFile(path)
+    local fileFind = file.Read(path) or ""
+    local dataFind = util.JSONToTable(fileFind) or {}
+    return dataFind
+end
+
+-- DIRECTORY DATA FOLDER
+if not file.Exists("data_scp263", "DATA") then
+    file.CreateDir("data_scp263")
+end
+
+--? We create the file if it don't exist with the default params, and we load the file when everythig is set up.
+for key, value in pairs(QuestionList) do
+    local FilePath= "data_scp263/" .. key .. SCP_263_CONFIG.NameFileQuestion
+    if not file.Exists(FilePath, "DATA") then
+        file.Write(FilePath, util.TableToJSON( value, true ))
+    end
+end
+
+--? We find every file store in data folder, and load every file
+local DataSCP263 = file.Find( "data_scp263/*.json" .. "*", "DATA" ) --? Get every json files
+for key, File in pairs(DataSCP263) do
+	local prefix = string.lower( string.Left( File, 2 ) ) --? Lang Prefix
+	SCP_263_CONFIG.QuestionList[prefix] = GetDataFromFile("data_scp263/" .. File)
+end
